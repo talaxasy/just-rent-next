4677d87f4fe6163249f0c8845d2d40f9c9803f81
Lev <talaxasy.or.lev@gmail.com>
2021-10-01 10:47:02 +0300

    remastered file system

--- server/dist/resolvers/validators/checkRegisterUser.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkRegisterUser = void 0;
-const checkRegisterUser = () => {
-};
-exports.checkRegisterUser = checkRegisterUser;
-//# sourceMappingURL=checkRegisterUser.js.map
